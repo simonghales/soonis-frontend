facf5058bb65203a1621f13361050ad9adc0c51b
simonghales <simonghales@gmail.com>
2016-06-16 00:35:06 +1000

    basic structure

---
+++ src/app/components/event-preview/event-preview-card.directive.js
@@ -0,0 +1,20 @@
+export class EventPreviewCard {
+  constructor () {
+    'ngInject';
+
+    this.templateUrl = 'app/components/event-preview/_event-preview-card.html';
+    this.scope = false;
+    this.restrict = 'E';
+    this.replace = true;
+
+  }
+
+  compile() {
+
+  }
+
+  link() {
+
+  }
+
+}
