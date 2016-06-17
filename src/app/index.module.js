068871a0ed9864b4dd1eca165d746814a947d056
simonghales <simonghales@gmail.com>
2016-06-18 00:53:20 +1000

    home page work

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -4,8 +4,22 @@ import { routerConfig } from './index.route';
 import { runBlock } from './index.run';
 import { MainController } from './main/main.controller';
 
-angular.module('soonisFrontend', ['ngAnimate', 'ngCookies', 'restangular', 'ui.router', 'toastr'])
+import { EventPreviewCard } from './components/event-preview/event-preview-card.directive.js';
+
+angular.module('soonisFrontend', [
+
+  'soonisFrontend.event',
+
+  'ngAnimate',
+  'ngCookies',
+  'restangular',
+  'ui.router',
+  'toastr'
+])
   .config(config)
   .config(routerConfig)
   .run(runBlock)
   .controller('MainController', MainController);
+
+angular.module('soonisFrontend.event', [])
+  .directive('eventPreviewCard', () => new EventPreviewCard);
